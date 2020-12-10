21f1d3dbbeb627b06924e0c7a1b13797b423dc23
Ulan Rakymzhanov <ulan@carta.healthcare>
2020-12-09 19:58:50 -0600

    update

--- src/App.js
+++ src/App.js
@@ -1,47 +1,29 @@
-import React, { Component, Suspense } from 'react';
+import React, { useState, Suspense } from 'react';
 import './App.css';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { removeToken, setToken } from './auth';
-// import Cookies from 'js-cookie';
 import Navbar from './components/Navbar';
 import Routes from './Routes';
 import Layout from './components/Layout';
-
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      userInfo: {},
-    };
-  }
-
-  logOut = () => {
-    // in production
-    // Cookies.remove('token');
-    // in development
-    this.setState({ customerName: '' });
+const App = () => {
+  const [userInfo, setUserInfo] = useState({});
+  const logOut = () => {
+    setUserInfo({ customerName: '' });
     removeToken();
   };
-
-  onLoginSubmit = (customer, token) => {
-    // only in development
+  const onLoginSubmit = (customer, token) => {
     setToken(customer, token);
-    // simply rerender
-    this.setState({ userInfo: customer });
+    setUserInfo({ userInfo: customer });
   };
-
-  render() {
-    return (
-      <Suspense fallback={<div>Loading...</div>}>
-        <Router>
-          <Navbar logOut={this.logOut} />
-          <Layout>
-            <Routes onLoginSubmit={this.onLoginSubmit} />
-          </Layout>
-        </Router>
-      </Suspense>
-    );
-  }
-}
-
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <Router>
+        <Navbar logOut={logOut} />
+        <Layout>
+          <Routes onLoginSubmit={onLoginSubmit} />
+        </Layout>
+      </Router>
+    </Suspense>
+  );
+};
 export default App;
